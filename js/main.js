b07196036b27af91e4964426b7159f984db9e9a9
freelancersazal <freelancersazal@gmail.com>
2024-03-21 21:59:45 +0600

    Home page updated

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 $(function () {
-    
-    "use strict";
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    "use strict";
+
+    var navoff = $('.main_menu').offset().top;
+    $(window).on("scroll", function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > navoff) {
+            $('.main_menu').addClass('sticky_menu');
+        } else {
+            $('.main_menu').removeClass('sticky_menu');
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
