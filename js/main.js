4190a6bcb5d3e9594558077c79d89697eced7cf8
freelancersazal <freelancersazal@gmail.com>
2024-03-22 22:38:28 +0600

    Home page updated

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(function () {
 
     "use strict";
 
+
+    // Menu fix js
     var navoff = $('.main_menu').offset().top;
     $(window).on("scroll", function () {
         var scrolling = $(this).scrollTop();
@@ -16,7 +18,45 @@ $(function () {
 
 
 
+    // Testimonial Slider
+    $('.testi_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        // variableWidth: true,
 
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 
 
 
